3b549d56ac1c8cdf4a16bf2fdeedb26e6067c3fa
Daniel Calderon <d_nni3l@pm.me>
2020-11-05 08:02:06 -0800

    woooooooah e30
    added v4 of e30m3 tree
    few more wheels as well
    sys9 -> mx license plate

--- personal/lua/autorun/simfphys_spawnlist_personal.lua
+++ personal/lua/autorun/simfphys_spawnlist_personal.lua
@@ -1,6 +1,6 @@
 local Category = "CycklPersonal"
 
--- BMW E30 M3 Tuned --
+-- BMW E30 M3 S55B30 --
 local V = {
 	Name = "BMW E30 M3 (S55B30)",
 	Model = "models/cyckl/e30m3/ext.mdl",
@@ -8,7 +8,8 @@ local V = {
 	Category = Category,
 	Members = {
 		ModelInfo = {
-			Color = Color(220, 20, 20),
+			Color = Color(0, 0, 0),
+			Bodygroups = {1},
 		},
 		Mass = 1700,
 		MaxHealth = 3000,
@@ -38,12 +39,12 @@ local V = {
 				model = "models/cyckl/e30m3/int.mdl",
 				color = Color(255, 255, 255, 255),
 				useVehicleColor = false,
-				pos = Vector(-0.25, 0.35, 2.5),
+				pos = Vector(0, 0, 0),
 				ang = Angle(0, 0, 0),
 				nosolid = true,
 			},
 			{
-				model = "models/cyckl/plates/ca.mdl",
+				model = "models/cyckl/plates/d2.mdl",
 				color = Color(255, 255, 255, 255),
 				useVehicleColor = false,
 				pos = Vector(-77.5, 0, 19),
@@ -51,10 +52,10 @@ local V = {
 				nosolid = true
 			},
 			{
-				model = "models/cyckl/plates/ca.mdl",
+				model = "models/cyckl/plates/d2.mdl",
 				color = Color(255, 255, 255, 255),
 				useVehicleColor = false,
-				pos = Vector(86, 0, 10),
+				pos = Vector(86, 0, 11),
 				ang = Angle(0, 0, 0),
 				nosolid = true
 			}
@@ -90,10 +91,10 @@ local V = {
 		BrakePower = 50,
 		IdleRPM = 750,
 		LimitRPM = 7500,
-		PeakTorque = 550,
+		PeakTorque = 400,
 		PowerbandStart = 2350,
 		PowerbandEnd = 5500,
-		Turbocharged = true,
+		Turbocharged = false,
 		PowerBias = 1,
 		EngineSoundPreset = 0,
 		Sound_Idle = "cyckl/cars/e30m3/e30_idle.wav",
@@ -115,7 +116,127 @@ local V = {
 		Gears = {-0.27, 0.00, 0.24, 0.43, 0.65, 0.85, 1.00, 1.18}
 	}
 }
-list.Set("simfphys_vehicles", "sim_fphys_e30m3_cyckl_tuned", V)
+list.Set("simfphys_vehicles", "sim_fphys_e30m3_cyckl_s55b30", V)
+
+-- BMW E30 M3 S55B30 Drift --
+local V = {
+	Name = "BMW E30 M3 (Drift)",
+	Model = "models/cyckl/e30m3/ext.mdl",
+	Class = "gmod_sent_vehicle_fphysics_base",
+	Category = Category,
+	Members = {
+		ModelInfo = {
+			Color = Color(0, 0, 0),
+			Bodygroups = {1},
+		},
+		Mass = 1700,
+		MaxHealth = 3000,
+		FuelFillPos = Vector(-58, -30, 20),
+		LightsTable = "e30m3",
+		CustomWheels = true,
+		CustomSuspensionTravel = 5,
+		CustomWheelModel = "models/cyckl/wheels/wedsa97.mdl",
+		CustomWheelPosFL = Vector(56, 28.5, 11),
+		CustomWheelPosFR = Vector(56, -28.5, 11),
+		CustomWheelPosRL = Vector(-43.2, 29.5, 11),
+		CustomWheelPosRR = Vector(-43.2, -29.5, 11),
+		CustomWheelAngleOffset = Angle(0, 90, 0),
+		CustomMassCenter = Vector(2, 0, -3),
+		CustomWheelCamber = -2,
+		CustomSteerAngle = 35,
+		SeatOffset = Vector(-1, -12, 34),
+		SeatPitch = -8,
+		SeatYaw = 90,
+		ExhaustPositions = {
+			{
+				pos = Vector(-83, 16, 4),
+				ang = Angle(0, 180, 0)
+			}
+		},
+		Attachments = {
+			{
+				model = "models/cyckl/e30m3/int.mdl",
+				color = Color(255, 255, 255, 255),
+				useVehicleColor = false,
+				pos = Vector(0, 0, 0),
+				ang = Angle(0, 0, 0),
+				nosolid = true,
+			},
+			{
+				model = "models/cyckl/plates/ca.mdl",
+				color = Color(255, 255, 255, 255),
+				useVehicleColor = false,
+				pos = Vector(-77, 0, 18.5),
+				ang = Angle(-12, 180, 0),
+				nosolid = true
+			},
+			{
+				model = "models/cyckl/plates/ca.mdl",
+				color = Color(255, 255, 255, 255),
+				useVehicleColor = false,
+				pos = Vector(84.5, 12, 10),
+				ang = Angle(0, 8, 0),
+				nosolid = true
+			}
+		},
+		PassengerSeats = {
+			{
+				pos = Vector(6, -14, 2),
+				ang = Angle(0, -90, 20)
+			},
+			{
+				pos = Vector(-20, 12, 2),
+				ang = Angle(0, -90, 20)
+			},
+			{
+				pos = Vector(-20, -12, 2),
+				ang = Angle(0, -90, 20)
+			},
+		  },
+		FrontHeight = 8,
+		FrontConstant = 200000,
+		FrontDamping = 4000,
+		FrontRelativeDamping = 4500,
+		RearHeight = 9,
+		RearConstant = 200000,
+		RearDamping = 4000,
+		RearRelativeDamping = 4500,
+		FastSteeringAngle = 10,
+		SteeringFadeFastSpeed = 535,
+		TurnSpeed = 8,
+		MaxGrip = 60,
+		Efficiency = 1.0,
+		GripOffset = 0,
+		BrakePower = 50,
+		IdleRPM = 750,
+		LimitRPM = 7500,
+		PeakTorque = 400,
+		PowerbandStart = 2350,
+		PowerbandEnd = 5500,
+		Turbocharged = true,
+		Backfire = true,
+		PowerBias = 1,
+		EngineSoundPreset = 0,
+		Sound_Idle = "cyckl/cars/e30m3/e30_idle.wav",
+        Sound_IdlePitch = 1,
+        Sound_IdleVolume = 2,
+		Sound_Mid = "cyckl/cars/e30m3/e30_on_5000.wav",
+		Sound_MidPitch = 1,
+		Sound_MidVolume = 1,
+		Sound_MidFadeOutRPMpercent = 100,
+		Sound_MidFadeOutRate = 1,
+		Sound_High = "cyckl/cars/e30m3/e30_on_8500.wav",
+		Sound_HighPitch = 1,
+		Sound_HighVolume = 1,
+		Sound_HighFadeInRPMpercent = 100,
+		Sound_HighFadeInRate = 1,
+		Sound_Throttle = "common/null.wav",
+        Sound_ThrottleVolume = 0,
+		DifferentialGear = 0.29,
+		Gears = {-0.27, 0.00, 0.24, 0.43, 0.65, 0.85, 1.00, 1.18}
+	}
+}
+list.Set("simfphys_vehicles", "sim_fphys_e30m3_cyckl_s55b30_drift", V)
 
 -- BMW E30 M3 Electric --
 local V = {
@@ -125,7 +246,7 @@ local V = {
 	Category = Category,
 	Members = {
 		ModelInfo = {
-			Color = Color(22, 22, 22),
+			Color = Color(64, 64, 64),
 		},
 		Mass = 1600,
 		MaxHealth = 3000,
@@ -150,12 +271,12 @@ local V = {
 				model = "models/cyckl/e30m3/int.mdl",
 				color = Color(255, 255, 255, 255),
 				useVehicleColor = false,
-				pos = Vector(-0.25, 0.35, 2.5),
+				pos = Vector(0, 0, 0),
 				ang = Angle(0, 0, 0),
 				nosolid = true,
 			},
 			{
-				model = "models/cyckl/plates/sys9.mdl",
+				model = "models/cyckl/plates/mx.mdl",
 				color = Color(255, 255, 255, 255),
 				useVehicleColor = false,
 				pos = Vector(-77.5, 0, 19),
@@ -163,7 +284,7 @@ local V = {
 				nosolid = true
 			},
 			{
-				model = "models/cyckl/plates/sys9.mdl",
+				model = "models/cyckl/plates/mx.mdl",
 				color = Color(255, 255, 255, 255),
 				useVehicleColor = false,
 				pos = Vector(86, 0, 11),
@@ -205,6 +326,9 @@ local V = {
 		PeakTorque = 300,
 		PowerbandStart = 0,
 		PowerbandEnd = 20000,
+		Turbocharged = true,
+		Supercharged = true,
+		DoNotStall = true,
 		PowerBias = 0.25,
 		EngineSoundPreset = 0,
 		EngineSoundPreset = 0,
@@ -228,6 +352,136 @@ local V = {
 	}
 }
 list.Set("simfphys_vehicles", "sim_fphys_e30m3_cyckl_electric", V)
+
+-- BMW E46 M3 --
+local V = {
+	Name = "BMW M3 E46",
+	Model = "models/DiggerCars/BMW_M3E46/v1.mdl",
+	Class = "gmod_sent_vehicle_fphysics_base",
+	Category = Category,
+	Members = {
+		ModelInfo = {
+			Color = Color(22, 22, 22),
+		},
+		Mass = 1341,
+		CustomWheels = true,
+		CustomSuspensionTravel = 15,
+		LightsTable = "m3e46",		
+		CustomWheelModel = "models/diggercars/bmw_m3e46csl/wheel.mdl",
+		CustomWheelPosFL = Vector(54.8,30,19),
+		CustomWheelPosFR = Vector(54.8,-30,19),
+		CustomWheelPosRL = Vector(-55.1,30,19),	
+		CustomWheelPosRR = Vector(-55.1,-30,19),
+		CustomWheelAngleOffset = Angle(0,-90,0),
+		
+		CustomMassCenter = Vector(2,0,-5),
+		
+		CustomSteerAngle = 35,
+		
+		SeatOffset = Vector(-7,-14.2,40),
+		SeatPitch = -10,
+		SeatYaw = 90,
+		
+		PassengerSeats = {
+			{
+				pos = Vector(-4,-15.5,7),
+				ang = Angle(0,-90,20)
+			}
+		},
+		ExhaustPositions = {
+			{
+				pos = Vector(-94,10,12),
+				ang = Angle(90,180,0)
+			},
+			{
+				pos = Vector(-94,14,12),
+				ang = Angle(90,180,0)
+			},
+			{
+				pos = Vector(-94,-10,12),
+				ang = Angle(90,180,0)
+			},
+			{
+				pos = Vector(-94,-14,12),
+				ang = Angle(90,180,0)
+			},
+		},
+		Attachments = {
+			{
+				model = "models/cyckl/plates/d2.mdl",
+				color = Color(255, 255, 255, 255),
+				useVehicleColor = false,
+				pos = Vector(-90, 0, 31),
+				ang = Angle(-10, 180, 0),
+				nosolid = true
+			},
+			{
+				model = "models/cyckl/plates/d2.mdl",
+				color = Color(255, 255, 255, 255),
+				useVehicleColor = false,
+				pos = Vector(86, 0, 17),
+				ang = Angle(0, 0, 0),
+				nosolid = true
+			}
+		},
+		
+		FrontHeight = 10,
+		FrontConstant = 179000,
+		FrontDamping = 3000,
+		FrontRelativeDamping = 5500,
+		
+		RearHeight = 10,
+		RearConstant = 179000,
+		RearDamping = 3000,
+		RearRelativeDamping = 5500,
+		
+		FastSteeringAngle = 10,
+		SteeringFadeFastSpeed = 535,
+		
+		TurnSpeed = 8,
+		
+		MaxGrip = 60,
+		Efficiency = 1.0,
+		GripOffset = -2,
+		BrakePower = 50,
+		
+		IdleRPM = 750,
+		LimitRPM = 7900,
+		PeakTorque = 269,
+		PowerbandStart = 4000,
+		PowerbandEnd = 6000,
+		
+		PowerBias = 1,
+		
+		EngineSoundPreset = 0, 
+
+		Sound_Idle = "simulated_vehicles/i6/ext_m3e92_idle.wav",
+		Sound_IdlePitch = 0.5,
+		
+		Sound_Mid = "simulated_vehicles/i6/ext_m3e92_on_8500.wav",
+		Sound_MidPitch = 0.65,
+		Sound_MidVolume = 1,
+		Sound_MidFadeOutRPMpercent = 100,		
+		Sound_MidFadeOutRate = 1,    
+
+		Sound_High = "simulated_vehicles/i6/ext_m3e92_on_4000.wav",
+		Sound_HighPitch = 0,
+		Sound_HighVolume = 0,
+		Sound_HighFadeInRPMpercent = 0,
+		Sound_HighFadeInRate = 0,
+		
+		Sound_Throttle = "simulated_vehicles/i6/ext_m3e92_on_4000.wav",		
+		Sound_ThrottlePitch = 0,
+		Sound_ThrottleVolume = 0,     
+
+		snd_blowoff = "simulated_vehicles/BlowOff/BOV-05.wav",            
+
+		
+		DifferentialGear = 0.28,
+		Gears = {-0.27,0.00,0.24,0.40,0.60,0.81,1.00,1.20}
+	}
+}
+list.Set( "simfphys_vehicles", "sim_fphys_e46m3_cyckl", V )	
 
 -- Porsche 911 (901) --
 local V = {
